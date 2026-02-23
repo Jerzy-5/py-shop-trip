62264345495d70db6501103b6c2c56385342527d
Jerzy Joniec <j.joniec5@gmail.com>
2026-02-23 12:57:33 +0100

    „final”

---
+++ app/customer.py
@@ -0,0 +1,74 @@
+from app.car import Car
+from math import sqrt
+
+
+class Customer:
+    def __init__(self, name: str,
+                 wanted_products: dict,
+                 location: list,
+                 money: float,
+                 car: Car) -> None:
+        self.name = name
+        self.wanted_products = wanted_products
+        self.location = location
+        self.money = money
+        self.car = car
+
+    def get_distance(self, shop: type) -> float:
+        cust_location = self.location
+        shop_location = shop.location
+        x1 = cust_location[0]
+        y1 = cust_location[1]
+        x2 = shop_location[0]
+        y2 = shop_location[1]
+        return sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+
+    def get_price(self, shop: type, fuel_price: float) -> float:
+        distance = self.get_distance(shop)
+        result = (((distance / 100) * self.car.fuel_consumption) * fuel_price)
+        return result * 2
+
+    def buy_cost(self, shop: type) -> float:
+        cost = 0
+        for keys, values in shop.products_available.items():
+            cost += shop.products_available[keys] * self.wanted_products[keys]
+        return cost
+
+    def trip_cost(self, shop: type, fuel_price: float) -> float:
+        fuel = self.get_price(shop, fuel_price)
+        products = self.buy_cost(shop)
+        return round(fuel + products, 2)
+
+    def printer(self, list_shops: list, fuel: float) -> None:
+        customer = self
+        print(f"{customer.name} has {customer.money} dollars")
+        for shop in list_shops:
+            print(f"{customer.name}'s trip to the"
+                  f" {shop.name} costs "
+                  f"{customer.trip_cost(shop, fuel)}")
+
+    def cheapest(self, list_shops: list, fuel: float) -> type:
+        cheapest_shop = list_shops[0]
+        cheapest = self.trip_cost(cheapest_shop, fuel)
+        for shop in list_shops[1:]:
+            shop_cost = self.trip_cost(shop, fuel)
+            if shop_cost < cheapest:
+                cheapest = shop_cost
+                cheapest_shop = shop
+        return cheapest_shop
+
+    def buy_at_shop(self, shop: type) -> None:
+        product_cart = self.wanted_products
+        shop_cart = shop.products_available
+        what = [k for k, v in product_cart.items()]
+        amount = [v for k, v in product_cart.items()]
+        price = [v for k, v in shop_cart.items()]
+        final = 0
+        for number in range(len(what)):
+            result = amount[number] * price[number]
+            if result.is_integer():
+                result = int(result)
+            print(f"{amount[number]} {what[number]}s for {result} dollars")
+            final += amount[number] * price[number]
+        print(f"Total cost is {final} dollars")
+        print("See you again!")
--- app/main.py
+++ app/main.py
@@ -1,3 +1,54 @@
-def shop_trip():
-    # write your code here
-    pass
+import json
+import datetime
+from app.car import Car
+from app.customer import Customer
+from app.shop import Shop
+import os
+
+
+def shop_trip() -> str:
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    config_path = os.path.join(base_dir, "config.json")
+    with open(config_path) as file:
+        data = json.load(file)
+
+    fuel = data["FUEL_PRICE"]
+    list_customers = []
+    for number in range(len(data["customers"])):
+        slownik = (data["customers"][number])
+        list_customers.append(Customer(slownik["name"],
+                                       slownik["product_cart"],
+                                       slownik["location"],
+                                       slownik["money"],
+                                       Car(slownik["car"]["brand"],
+                                           slownik["car"]
+                                           ["fuel_consumption"])))
+    list_shops = []
+    for number in range(len(data["shops"])):
+        slownik = (data["shops"][number])
+        dictionary = (data["shops"][number])
+        list_shops.append(Shop(dictionary["name"],
+                               dictionary["location"],
+                               dictionary["products"]))
+    for customer in list_customers:
+        customer.printer(list_shops, fuel)
+        cheapest_shop = customer.cheapest(list_shops, fuel)
+        if customer.money < customer.trip_cost(cheapest_shop, fuel):
+            print(f"{customer.name}"
+                  f" doesn't have enough money to"
+                  f" make a purchase in any shop")
+            continue
+        print(f"{customer.name} rides to {cheapest_shop.name}")
+        print("")
+        current_date = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+
+        print(f"Date: {current_date}")
+        print(f"Thanks, {customer.name}, for your purchase!")
+        print("You have bought:")
+
+        customer.money -= customer.trip_cost(cheapest_shop, fuel)
+        customer.buy_at_shop(cheapest_shop)
+        print("")
+        print(f"{customer.name} rides home")
+        print(f"{customer.name} now has {customer.money} dollars")
+        print("")
